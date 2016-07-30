0cdf5ff783d84d3210dde6cd0457d8276ed6c311
fernando <nanocatdemen@gmail.com>
2016-07-30 19:09:37 -0400

    Mas cosas de JIMBO

--- src/resiliencemetrics/admin.py
+++ src/resiliencemetrics/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
-from .forms import PaperForm
-from .models import Paper
+# from .forms import PaperForm
+from .models import *
 
-class PaperAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "doi"]
-    form = PaperForm
 
-admin.site.register(Paper, PaperAdmin)
+admin.site.register(Paper)
+admin.site.register(Experiment)
+admin.site.register(Metric)
+admin.site.register(Source)
+admin.site.register(NetworkModel)
+admin.site.register(QualityStudy)
+admin.site.register(ExperimentHasNetworkModel)
+admin.site.register(ExperimentComparedToMetric)
+admin.site.register(Application)
+admin.site.register(MetricHasApplication)
+admin.site.register(MetricType)
+admin.site.register(MetricsHasType)
+admin.site.register(PaperHasMetric)
+admin.site.register(Topic)
+admin.site.register(PaperHasTopic)
+admin.site.register(Author)
+admin.site.register(PaperHasAuthor)
